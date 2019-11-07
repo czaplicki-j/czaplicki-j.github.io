5cb37c6e1c048ab714975ca492d9cd894fb68431
czaplicki-j <57410461+czaplicki-j@users.noreply.github.com>
2019-11-07 14:52:07 -0500

    Update powerNumGenerator.js

--- js/powerNumGenerator.js
+++ js/powerNumGenerator.js
@@ -3,4 +3,4 @@ for(j=0;j<5;j++){
 document.write(Math.round(Math.random() * 69))
 document.write(" ")
 }
-document.write(Math.round(Math.random() * 26))
+document.write(Math.round(Math.random() * 26));
