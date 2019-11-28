72015d25e1f792ba50b6733b9aa958df55b6d88a
czaplicki-j <57410461+czaplicki-j@users.noreply.github.com>
2019-11-28 17:58:33 -0500

    .

--- js/powerNumGenerator.js
+++ js/powerNumGenerator.js
@@ -1,7 +1,7 @@
 var j;
 var min = 1;
-var max = 69;
-var power = 26;
+var max = 68;
+var power = 25;
 for(j=0;j<5;j++){
 document.write(Math.round(Math.random() * ((max - min) + 1))+min)
 document.write(" ")
--- js/powerPlayGen.js
+++ js/powerPlayGen.js
@@ -1,5 +1,5 @@
 var j;
 var min = 1;
-var max = 5;
+var max = 4;
 document.write("x")
 document.write(Math.round(Math.random() * ((max - min) + 1))+min)
